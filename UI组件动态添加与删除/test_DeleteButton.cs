1d8432fb5933094203b9c86eee98f5dd7a55ea64
Dr-Don <15107787001@sina.cn>
2020-02-17 15:37:40 +0800

    UI组件动态添加与删除

---
+++ UI组件动态添加与删除/test_DeleteButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test_DeleteButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void delete()
+    {
+        gameObject.GetComponent<LayoutElement>().ignoreLayout = true;
+        Destroy(gameObject);
+    }
+}
---
+++ UI组件动态添加与删除/test_GenerateButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_GenerateButton : MonoBehaviour
+{
+    public GameObject btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //or load the button from a folder
+        //btn = (GameObject)Resources.Load<GameObject>("Button1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject go = Instantiate(btn);
+            go.transform.SetParent(transform);
+        }
+    }
+}
